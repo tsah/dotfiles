09a1e1d6171fb4068ac05e474205ecb595105450
tsah-alike <tsah@alike.health>
2022-11-17 10:10:50 +0200

    Add some more handy aliases

--- ts.sh
+++ ts.sh
@@ -7,6 +7,7 @@ SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 # Only create tmux session if it doesn't already exist
 if [ "$SESSIONEXISTS" = "" ]
 then
+    echo 'Session not found'
     # Start New Session with our name
     tmux new-session -d -s $SESSION
 
