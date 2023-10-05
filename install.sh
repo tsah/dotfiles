db13aea6e76c7147ead0de830d8795a7a3d1d6f9
tsah-alike <tsah@alike.health>
2023-10-05 14:29:21 +0300

    fix weird zshrc bug

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ln -sf ~/dotfiles/nvim ~/.config/nvim
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
