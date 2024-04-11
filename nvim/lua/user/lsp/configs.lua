85397f746e1d864ef5754233f623013aa1da8107
tsah-alike <tsah@alike.health>
2024-04-11 11:46:13 +0300

    Add html LSP

--- nvim/lua/user/lsp/configs.lua
+++ nvim/lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "pyright", "ruff_lsp"}
+local servers = { "jsonls", "pyright", "ruff_lsp", "html"}
 
 lsp_installer.setup {
 	ensure_installed = servers
