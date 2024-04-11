814f028ceca18fd746370868ad5d61ba969a0782
tsah-alike <tsah@alike.health>
2024-04-11 11:46:24 +0300

    Add copilot support

--- nvim/lua/user/copilot.lua
+++ nvim/lua/user/copilot.lua
@@ -1,4 +1,14 @@
 require("copilot").setup({
-  suggestion = { enabled = false },
-  panel = { enabled = false },
+  suggestion = { enabled = true },
+  panel = { enabled = true },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    keymap= {
+      accept = "<C-l>",
+      accept_word = "<C-h>",
+      next = "<M-j>",
+      prev = "<M-k>",
+    }
+  },
 })
