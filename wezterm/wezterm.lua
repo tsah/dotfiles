58ed71b77245f4309c988dbd5ba95bcfc5820f3d
Tsah Weiss <tsah.weiss@gmail.com>
2025-09-14 22:36:10 +0300

    update terminal emulator configurations
    - Ghostty: Add keybindings for tab management (Alt+T/W) and file opening (Alt+J)
    - WezTerm: Switch to CaskaydiaMono font, enable tab bar, disable Wayland, remove custom keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,8 +16,9 @@ local act = wezterm.action
 
 local config = {
 
+    enable_wayland=false,
 	font_size = 20,
-	font = wezterm.font "Hack Nerd Font",
+	font = wezterm.font "CaskaydiaMono Nerd Font",
   front_end = 'WebGpu',
   color_scheme = 'Catppuccin Mocha',
 
@@ -32,89 +33,13 @@ local config = {
 	-- general options
 	adjust_window_size_when_changing_font_size = false,
 	debug_key_events = false,
-	enable_tab_bar = false,
+	enable_tab_bar = true,
 	native_macos_fullscreen_mode = false,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
 
 	-- keys
-	keys = {
-		k.cmd_key(".", k.multiple_actions(":ZenMode")),
-		k.cmd_key("[", act.SendKey({ mods = "CTRL", key = "o" })),
-		k.cmd_key("]", act.SendKey({ mods = "CTRL", key = "i" })),
-		k.cmd_key("q", k.multiple_actions(":qa!")),
-		k.cmd_to_tmux_prefix("1", "1"),
-		k.cmd_to_tmux_prefix("2", "2"),
-		k.cmd_to_tmux_prefix("3", "3"),
-		k.cmd_to_tmux_prefix("4", "4"),
-		k.cmd_to_tmux_prefix("5", "5"),
-		k.cmd_to_tmux_prefix("6", "6"),
-		k.cmd_to_tmux_prefix("7", "7"),
-		k.cmd_to_tmux_prefix("8", "8"),
-		k.cmd_to_tmux_prefix("9", "9"),
-		k.cmd_to_tmux_prefix("`", "n"),
-		k.cmd_to_tmux_prefix("b", "B"),
-		k.cmd_to_tmux_prefix("C", "C"),
-		k.cmd_to_tmux_prefix("d", "D"),
-		k.cmd_to_tmux_prefix("G", "G"),
-		k.cmd_to_tmux_prefix("g", "g"),
-		k.cmd_to_tmux_prefix("j", "T"),
-		k.cmd_to_tmux_prefix("l", "L"),
-		k.cmd_to_tmux_prefix("n", '"'),
-		k.cmd_to_tmux_prefix("N", "%"),
-		k.cmd_to_tmux_prefix("o", "u"),
-		k.cmd_to_tmux_prefix("T", "!"),
-		k.cmd_to_tmux_prefix("t", "c"),
-		k.cmd_to_tmux_prefix("w", "x"),
-		k.cmd_to_tmux_prefix("z", "z"),
-
-		k.cmd_key(
-			"s",
-			act.Multiple({
-				act.SendKey({ key = "\x1b" }), -- escape
-				k.multiple_actions(":w"),
-			})
-		),
-
-		{
-			mods = "CTRL",
-			key = "Tab",
-			action = act.Multiple({
-				act.SendKey({ mods = "CTRL", key = "b" }),
-				act.SendKey({ key = "n" }),
-			}),
-		},
-
-		{
-			mods = "CTRL|SHIFT",
-			key = "Tab",
-			action = act.Multiple({
-				act.SendKey({ mods = "CTRL", key = "b" }),
-				act.SendKey({ key = "n" }),
-			}),
-		},
-
-		{
-			mods = "CMD",
-			key = "~",
-			action = act.Multiple({
-				act.SendKey({ mods = "CTRL", key = "b" }),
-				act.SendKey({ key = "p" }),
-			}),
-		},
-    {
-      mods = "OPT",
-      key = "LeftArrow",
-      action = wezterm.action({SendString="\x1bb"}),
-    },
-    {
-      mods = "OPT",
-      key = "RightArrow",
-      action = wezterm.action{SendString="\x1bf"},
-    }
-	},
 }
-
 wezterm.on("user-var-changed", function(window, pane, name, value)
 	-- local appearance = window:get_appearance()
 	-- local is_dark = appearance:find("Dark")
